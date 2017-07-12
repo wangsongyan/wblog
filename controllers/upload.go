799fd8004c13445033030e6b87390dfe9739fdc7
wangsongyan <1104237534@qq.com>
2017-07-12 16:37:07 +0800

    上传文件到七牛云服务器

---
+++ controllers/upload.go
@@ -0,0 +1,84 @@
+package controllers
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/wangsongyan/wblog/system"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"qiniupkg.com/api.v7/conf"
+	"qiniupkg.com/api.v7/kodo"
+	"qiniupkg.com/api.v7/kodocli"
+)
+
+// 获取文件大小的接口
+type Size interface {
+	Size() int64
+}
+
+// 获取文件信息的接口
+type Stat interface {
+	Stat() (os.FileInfo, error)
+}
+
+// 构造返回值字段
+type PutRet struct {
+	Hash string `json:"hash"`
+	Key  string `json:"key"`
+}
+
+func Upload(c *gin.Context) {
+	var err error
+	file, _, err := c.Request.FormFile("file")
+	if err == nil {
+		var key string
+		key, err = uploadFile(file)
+		if err == nil {
+			c.JSON(http.StatusOK, gin.H{
+				"url": system.GetConfiguration().QiniuFileServer + key,
+			})
+			return
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": err.Error(),
+	})
+}
+
+func uploadFile(file multipart.File) (string, error) {
+
+	conf.ACCESS_KEY = system.GetConfiguration().QiniuAccessKey
+	conf.SECRET_KEY = system.GetConfiguration().QiniuSecretKey
+
+	// 创建一个Client
+	c := kodo.New(0, nil)
+	// 设置上传的策略
+	policy := &kodo.PutPolicy{
+		Scope: system.GetConfiguration().QiniuBucket,
+		//设置Token过期时间
+		Expires: 3600,
+	}
+	// 生成一个上传token
+	token := c.MakeUptoken(policy)
+	// 构建一个uploader
+	zone := 0
+	uploader := kodocli.NewUploader(zone, nil)
+
+	var size int64
+	if statInterface, ok := file.(Stat); ok {
+		fileInfo, _ := statInterface.Stat()
+		size = fileInfo.Size()
+	}
+	if sizeInterface, ok := file.(Size); ok {
+		size = sizeInterface.Size()
+	}
+
+	var ret PutRet
+	err := uploader.PutWithoutKey(nil, &ret, token, file, size, nil)
+	if err != nil {
+		fmt.Println("io.Put failed:", err)
+		return "", err
+	}
+	return ret.Key, nil
+}
--- system/system.go
+++ system/system.go
@@ -7,9 +7,11 @@ import (
 )
 
 type Configuration struct {
-	SignupEnabled   bool   `yaml:"signup_enabled"` // signup enabled or not
-	QiniuAppKey     string `yaml:"qiniu_appkey"`   // qiniu
-	QiniuAppSecret  string `yaml:"qiniu_appsecret"`
+	SignupEnabled   bool   `yaml:"signup_enabled"`  // signup enabled or not
+	QiniuAccessKey  string `yaml:"qiniu_accesskey"` // qiniu
+	QiniuSecretKey  string `yaml:"qiniu_secretkey"`
+	QiniuFileServer string `yaml:"qiniu_fileserver"`
+	QiniuBucket     string `yaml:"qiniu_bucket"`
 	GithubAppKey    string `yaml:"github_appkey"` // github
 	GithubAppSecret string `yaml:"github_appsecret"`
 	SmtpUsername    string `yaml:"smtp_username"`  // username
