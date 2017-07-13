995e2ace0fc373b21fa53d2025531f247b1a13d7
wangsongyan <1104237534@qq.com>
2017-07-13 13:54:06 +0800

    注册页面、登录页面改版

--- models/models.go
+++ models/models.go
@@ -59,7 +59,7 @@ type User struct {
 	VerifyState   string    `gorm:"default:'0'"`               //邮箱验证状态
 	SecretKey     string    `gorm:"default:null"`              //密钥
 	OutTime       time.Time //过期时间
-	GithubLoginId string    `gorm:"unique_index"` // github唯一标识
+	GithubLoginId string    `gorm:"unique_index;default:null"` // github唯一标识
 	IsAdmin       bool      //是否是管理员
 	AvatarUrl     string    // 头像链接
 	NickName      string    // 昵称
