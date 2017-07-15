5492ab1fdca7ac447f7f91deb87dbd9980e7b21e
wangsongyan <1104237534@qq.com>
2017-07-15 14:23:49 +0800

    更新依赖项目，文章列表内容简介样式修改

--- controllers/tag.go
+++ controllers/tag.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday"
 	"github.com/wangsongyan/wblog/models"
 	"net/http"
 	"strconv"
@@ -26,8 +28,10 @@ func TagGet(c *gin.Context) {
 	id := c.Param("id")
 	posts, err := models.ListPost(id)
 	if err == nil {
+		policy := bluemonday.StrictPolicy()
 		for _, post := range posts {
 			post.Tags, _ = models.ListTagByPostId(strconv.FormatUint(uint64(post.ID), 10))
+			post.Body = policy.Sanitize(string(blackfriday.MarkdownCommon([]byte(post.Body))))
 		}
 		c.HTML(http.StatusOK, "index/index.html", gin.H{
 			"posts":    posts,
