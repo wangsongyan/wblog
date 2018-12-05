760aa6dd1bf85a0a7eabe97709fe6c32e1685d83
wangsongyan <1104237534@qq.com>
2018-12-05 17:15:25 +0800

    add maxread and maxcomment

--- controllers/index.go
+++ controllers/index.go
@@ -45,14 +45,16 @@ func IndexGet(c *gin.Context) {
 	}
 	user, _ := c.Get(CONTEXT_USER_KEY)
 	c.HTML(http.StatusOK, "index/index.html", gin.H{
-		"posts":     posts,
-		"tags":      models.MustListTag(),
-		"archives":  models.MustListPostArchives(),
-		"links":     models.MustListLinks(),
-		"user":      user,
-		"pageIndex": pageIndex,
-		"totalPage": int(math.Ceil(float64(total) / float64(pageSize))),
-		"path":      c.Request.URL.Path,
+		"posts":           posts,
+		"tags":            models.MustListTag(),
+		"archives":        models.MustListPostArchives(),
+		"links":           models.MustListLinks(),
+		"user":            user,
+		"pageIndex":       pageIndex,
+		"totalPage":       int(math.Ceil(float64(total) / float64(pageSize))),
+		"path":            c.Request.URL.Path,
+		"maxReadPosts":    models.MustListMaxReadPost(),
+		"maxCommentPosts": models.MustListMaxCommentPost(),
 	})
 }
 
