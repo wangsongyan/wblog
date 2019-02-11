c0c7deb184d50bea51ed79c212a867317596bbba
wangsongyan <1104237534@qq.com>
2019-02-11 13:10:30 +0800

    replace qiniu by smms

--- controllers/uploader.go
+++ controllers/uploader.go
@@ -3,5 +3,5 @@ package controllers
 import "mime/multipart"
 
 type Uploader interface {
-	upload(file multipart.File) (string, error)
+	upload(file multipart.File, fileHeader *multipart.FileHeader) (string, error)
 }
